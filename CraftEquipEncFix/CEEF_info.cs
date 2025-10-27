9945a4cb2210909409223250aae7b2aeb1cf83c4
InuiDame <928231678@qq.com>
2025-10-27 23:51:20 +0800

    Modify structure

---
+++ CraftEquipEncFix/CEEF_info.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+
+namespace CEEF_info
+{
+    [BepInPlugin("inui.noadashabi.CEEF.info", "CraftEquipEncFix", "1.0.0")]
+    public class CraftEquipEncFix : BaseUnityPlugin
+    {
+
+        private void Start()
+        {
+            new Harmony("CraftEquipEncFix").PatchAll();
+            base.Logger.LogInfo("附魔修复已经激活");
+        }
+    }
+}
--- Independent small module MODs/README.md
+++
@@ -1,3 +0,0 @@
-# Independent small module MODs
-
-### The MODs here are some small modules that can be used independently.
