77fdb44466eae65aac9f5c5e4cf242143c7867ee
InuiDame <928231678@qq.com>
2025-11-18 17:46:53 +0800

    i can see you

---
+++ Icanseeanybody/Icanseeanybody.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+using BepInEx;
+using Cwl.Helper.Unity;
+using HarmonyLib;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace Icanseeanybody.spell
+{
+
+   [HarmonyPatch(typeof(Chara), "Refresh")]
+   public class Chara_Refresh_Patch
+   {
+       [HarmonyPostfix]
+       public static void Postfix(Chara __instance)
+       {
+        // 在原始方法执行后，强制设置 visibleWithTelepathy 为 true
+        __instance.visibleWithTelepathy = true;
+        
+       } 
+   }
+
+
+}
+
+namespace Icanseeanybody.info
+{
+
+
+    [BepInPlugin("Icanseeanybody", "心灵感应拓展模块", "1.0.0")]
+    internal class MetalSlash : BaseUnityPlugin
+    {
+        private void Start() {
+            Harmony harmony = new Harmony("inui.Icanseeanybody");
+            harmony.PatchAll();
+            Logger.LogInfo("心灵感应拓展模块已加载");
+        }
+    }
+}
---
+++ Icanseeanybody/README.md
@@ -0,0 +1,4 @@
+# I can see anybody
+Mod Url ———— https://steamcommunity.com/sharedfiles/itemedittext/?id=3607957888
+
+This MOD modifies the Telepathy effect to work on all types of creatures. You can now additionally see mechanical, undead, demonic, and other units.
